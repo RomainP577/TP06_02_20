d8026c46e9e101ce595b2fbbf65c0b8c65ab87e1
Payet Romain <romain.topp@gmail.com>
2020-02-06 10:19:50 +0100

    gestion leiu

--- jdvelh.c
+++ jdvelh.c
@@ -6,46 +6,63 @@ struct Lieu
 {
     char Name[64];
 	int LieuExistant;
+	char Description[64];
+	int GoldGain;
+	int HPGain;
+	int ID[5];
 };
 
 typedef struct Lieu Lieu;
 
+int Travel = 1;
+
 int main (){
 
-	//vars
-	int ChoixLieu;
-	Lieu a_Lieux[4];
-	int i;
-
-	//Declaration lieux
-	Lieu Lieu1 = {"Lieu1", 1};
-	Lieu Lieu2 = {"Lieu2", 1};
-	Lieu Lieu3 = {"Lieu3", 1};
-	Lieu Lieu4 = {"Lieu4", 1};
-	
-	//Array lieux
-	a_Lieux[0] = Lieu1;
-	a_Lieux[1] = Lieu2;
-	a_Lieux[2] = Lieu3;
-	a_Lieux[3] = Lieu4;
-	
-	for (i=0; i<4; i++)
+	while (Travel=1)
 	{
-		if (a_Lieux[i].LieuExistant==1)
+			//vars
+		int ChoixLieu;
+		Lieu a_Lieux[4];
+		int i;
+
+		//Declaration lieux
+		Lieu Lieu1 = {"Lieu1", 1, "Lieu1Details", 2, 3, 0};
+		Lieu Lieu2 = {"Lieu2", 1, "Lieu1Details", 2, 3, 0};
+		Lieu Lieu3 = {"Lieu3", 1, "Lieu1Details", 2, 3, 0};
+		Lieu Lieu4 = {"Lieu4", 1, "Lieu1Details", 2, 3, 0};
+		
+		//Array lieux
+		a_Lieux[0] = Lieu1;
+		a_Lieux[1] = Lieu2;
+		a_Lieux[2] = Lieu3;
+		a_Lieux[3] = Lieu4;
+		
+		for (i=0; i<4; i++)
 		{
-			printf("-%s \n", a_Lieux[i].Name);
+			if (a_Lieux[i].LieuExistant==1)
+			{
+				printf("-%s \n", a_Lieux[i].Name);
+			}
+		}
+		
+		//Affichage txt choix lieux
+		printf("Ou voulez vous aller ?\n");
+		scanf("%d", &ChoixLieu);
+		ChoixLieu -= 1;
+		
+		/*if (a_Lieux[i].LieuExistant!=1)
+		{
+			printf("Lieu inexistant");
+			scanf("%d", &ChoixLieu);
+		}*/
+		
+		printf("Lieu choisi : %d", ChoixLieu);
+		
+		//CONDITIONS DEPLACEMENT
+		if (a_Lieux[ChoixLieu].LieuExistant==1)
+		{
+			printf("Vous etes arrive au %s", a_Lieux[ChoixLieu].Name);
 		}
 	}
 	
-	//Affichage txt choix lieux
-	printf("Ou voulez vous aller ?");
-	scanf("%d", &ChoixLieu);
-	ChoixLieu -= 1;
-	printf("Lieu choisi : %d", ChoixLieu);
-	
-	//CONDITIONS DEPLACEMENT
-	if (a_Lieux[ChoixLieu].LieuExistant==1)
-	{
-		printf("Vous etes arrive au %s", a_Lieux[ChoixLieu].Name);
-	}
 }
